bd2e311670f9b4a18c1fd907db15500bdd96b7ac
Manish Warang <manish6385@gmail.com>
2021-06-03 16:08:50 +0530

    adding state locking via DynamoDB

--- remote-backend.tf
+++ remote-backend.tf
@@ -3,5 +3,7 @@ terraform {
     bucket = "web-app-terraform-demo-6385"
     key    = "terraform-tfstate"
     region = "us-east-1"
+    encrypt = true
+    dynamodb_table = "terraform-lock-state"
   }
 }
